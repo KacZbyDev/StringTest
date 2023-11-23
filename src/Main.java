9c0a624e8e1b2eb4e07e2b8e64ce70b0b2586a42
Kacper <maustriacki@gmail.com>
2023-11-23 13:27:06 +0100

    drugi commitz

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        String test = scanner.nextLine();
+        String middle = "";
+        String beggin = "";
+        String end = "";
+        if( test.length() > 2) {
+            middle = test.substring((test.length() /2)-1, (test.length()/2) +1);
+            beggin = test.substring(0,test.length()/2 -1);
+            end = middle +  beggin + test.substring((test.length()/2) +1);
+        }
+        System.out.println(end);
     }
+
 }
